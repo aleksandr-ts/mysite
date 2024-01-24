c4fe9ec72a0d1f4dc98c81322616a200728565bf
Aleksandr T <a61920025@gmail.com>
2024-01-24 14:17:34 +0300

    feat: added tagging posts

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from .models import Post, Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
-
+from taggit.models import Tag
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @require_POST
 def post_comment(request, post_id):
@@ -71,8 +72,28 @@ def post_detail(request, year, month, day, post):
                    'form': form})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+        # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 
+                  'blog/post/list.html',
+                  {'posts': posts,
+                  'tag': tag})
+
+
